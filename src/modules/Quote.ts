78aef272cdb3775b8ba1bc0a9d6cd45d591cf1b0
jgarcia0092 <johngarcia0092@gmail.com>
2023-05-22 18:33:13 -0400

    feat: add types to the quote module

--- src/modules/Quote.ts
+++ src/modules/Quote.ts
@@ -1,6 +1,8 @@
+import { LorQuote, LorResponse } from '@/types';
 import { BaseModule } from './BaseModule';
 
 /**
+ * @class
  * @name Quote
  * @description the module that manages endpoints under `/quote`.
  */
@@ -9,11 +11,25 @@ export class Quote extends BaseModule {
         super(accessToken);
     }
 
+    /**
+     * @function getAll
+     * @description get a quote list.
+     * @param {number} limit
+     * @param {number} offset
+     */
     getAll(limit: number = 1000, offset: number = 0) {
-        return this.theOneAPI.get('/quote', { params: { limit, offset } }).then(this.processResponse);
+        return this.theOneAPI.get<LorResponse<LorQuote>>('/quote', { params: { limit, offset } })
+            .then(this.processResponse);
     }
 
-    getById = (id: string) => {
-        return this.theOneAPI.get(`/quote/${id}`).then(this.processResponse);
+    /**
+     * @function getById
+     * @description get a quote by id.
+     * @param {string} id
+     */
+    getById(id: string) {
+        return this.theOneAPI.get<LorResponse<LorQuote>>(`/quote/${id}`)
+            .then((res) => res.data)
+            .then((res) => res.docs?.[0]);
     }
 }
